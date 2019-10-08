f730e009f3bf9f1386fdd33d3463f21193d54c0e
applecookie <orccookies@gmail.com>
2019-10-08 15:49:07 +0300

    Team & video carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 $(document)
     .ready(() => {
-        // team slider
-        $('.slikparnt')
+        // video carousel
+        $('.video-list')
+            .slick({
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                }],
+            });
+        // team carousel
+        $('.team-list')
             .slick({
                 infinite: true,
                 slidesToShow: 4,
