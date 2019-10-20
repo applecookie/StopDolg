0e05a7c8f34abd679e3df7e60fc10ab491bd5f76
applecookie <orccookies@gmail.com>
2019-10-21 02:36:39 +0300

    Insolvency managers

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 $(document)
     .ready(() => {
         // video carousel
+        $('.profile-list')
+            .slick({
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                }],
+            });
+        // video carousel
         $('.video-list')
             .slick({
                 infinite: true,
