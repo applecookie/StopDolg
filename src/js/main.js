11b477ea203d44e776a58bb375ea8d664a66c9cb
applecookie <orccookies@gmail.com>
2020-04-03 15:27:34 +0300

    Insolvency defence blockquote alert video expert hammer list fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,82 @@
 $(document)
     .ready(() => {
-        // $('.profile-list')
-        //     .slick({
-        //         infinite: true,
-        //         slidesToShow: 2,
-        //         slidesToScroll: 1,
-        //         responsive: [{
-        //             breakpoint: 959,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //             },
-        //         }],
-        //     });
-        // $('.mass-media__list')
-        //     .slick({
-        //         infinite: true,
-        //         slidesToShow: 5,
-        //         variableWidth: true,
-        //         responsive: [{
-        //             breakpoint: 1024,
-        //             settings: {
-        //                 slidesToShow: 4,
-        //                 variableWidth: false,
-        //             },
-        //         }, {
-        //             breakpoint: 768,
-        //             settings: {
-        //                 slidesToShow: 3,
-        //                 variableWidth: false,
-        //             },
-        //         }, {
-        //             breakpoint: 640,
-        //             settings: {
-        //                 slidesToShow: 2,
-        //                 variableWidth: false,
-        //             },
-        //         }, {
-        //             breakpoint: 480,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //                 variableWidth: false,
-        //             },
-        //         }],
-        //     });
-        // $('.video-list')
-        //     .slick({
-        //         infinite: true,
-        //         slidesToShow: 2,
-        //         slidesToScroll: 1,
-        //         responsive: [{
-        //             breakpoint: 768,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //             },
-        //         }],
-        //     });
-        // $('.team-list')
-        //     .slick({
-        //         infinite: true,
-        //         slidesToShow: 4,
-        //         slidesToScroll: 1,
-        //         responsive: [{
-        //             breakpoint: 960,
-        //             settings: {
-        //                 slidesToShow: 3,
-        //             },
-        //         }, {
-        //             breakpoint: 640,
-        //             settings: {
-        //                 slidesToShow: 2,
-        //             },
-        //         }, {
-        //             breakpoint: 480,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //             },
-        //         }],
-        //     });
+        $('.profile-list')
+            .slick({
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 959,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                }],
+            });
+        $('.mass-media__list')
+            .slick({
+                infinite: true,
+                slidesToShow: 5,
+                variableWidth: true,
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        variableWidth: false,
+                    },
+                }, {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3,
+                        variableWidth: false,
+                    },
+                }, {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 2,
+                        variableWidth: false,
+                    },
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        variableWidth: false,
+                    },
+                }],
+            });
+        $('.video-list')
+            .slick({
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                }],
+            });
+        $('.team-list')
+            .slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 960,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                }, {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                }],
+            });
         // faq accordion
         $('.faq-list__toggle')
             .on('click', function () {
